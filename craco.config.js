148d85d2c1eb1a4ef4d59fbaccd9be29cf46b7ff
Maria Marinescu <maria@voxalyze.com>
2024-02-17 16:12:01 +0100

    Fix build & remove unused dependecies

--- craco.config.js
+++ craco.config.js
@@ -5,14 +5,6 @@
       plugins: [
         {
           plugin: VitePlugin,
-          // Optional: Specify the path to the Vite configuration file.
-          // If not specified, the default Vite configuration file will be used.
-          // viteConfigFile: 'vite.config.js',
         },
       ],
-      style: {
-        postcssOptions: {
-            plugins: [require('tailwindcss'), require('autoprefixer')],
-        },
-    },
     };
